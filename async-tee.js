ca2e7448468a01fde56371b9f81bddfdfe4c49ee
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-04-20 19:54:48 -0400

    Begin impl.

--- async-tee.js
+++ async-tee.js
@@ -1,24 +1,36 @@
 import immediate from "p-immediate"
+import Deferrant from "deferrant"
 
-export function asyncIteratorTee( asyncIter, { notify: true}= {}){
+export function asyncIteratorTee( asyncIter, { notify= false, signal}= {}){
 	const self= this instanceof asyncIteratorTee? this: {}
 	self.state= []
-	self[ Symbol.iterator]: function(){
+	self[ Symbol.iterator]= function(){
 		const iteration= self.state[ Symbol.iterator]
 		if( iteration){
 			return iteration.call( self.state)
 		}
 	}
-
-	self.fork= function( ){
-		
-	}
-	self.next= function( arg){
-		
+	if( notify){
+		self.notify= Deferrant()
 	}
 
+	self.next= async function( arg){
+		const next= await asyncIter.next()
+		self.state.push( next.value)
+		self.value= next.value
+		self.done= next.done
+		if( notify){
+			self.notify.resolve( next.value)
+			if( !next.done){
+				self.notify= Deferrant()
+			}
+		}
+		return self
+	}
+	self.value= null
+	self.done= false
 
-	immediate().then(
+	//self.fork= function( ){}
 
 	return self
 }
