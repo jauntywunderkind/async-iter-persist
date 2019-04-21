0b85baba10e34ee3861bb172ac83a81e35ab90dc
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-04-20 20:18:21 -0400

    Reorganize and comment.

--- async-tee.js
+++ async-tee.js
@@ -3,34 +3,48 @@ import Deferrant from "deferrant"
 
 export function asyncIteratorTee( asyncIter, { notify= false, signal}= {}){
 	const self= this instanceof asyncIteratorTee? this: {}
+
+	// state is the existing data that's been seen, the 'tee'
 	self.state= []
-	self[ Symbol.iterator]= function(){
-		const iteration= self.state[ Symbol.iterator]
-		if( iteration){
-			return iteration.call( self.state)
-		}
-	}
+	// notify is a rotating signals listeners that there is new data
 	if( notify){
 		self.notify= Deferrant()
 	}
 
+	// pass through iterator values
+	self.value= null
+	self.done= false
+	// implement iterator next
 	self.next= async function( arg){
+		// get next value
 		const next= await asyncIter.next()
-		self.state.push( next.value)
-		self.value= next.value
+		// pass through next value
 		self.done= next.done
-		if( notify){
-			self.notify.resolve( next.value)
-			if( !next.done){
-				self.notify= Deferrant()
-			}
+		self.value= next.value
+
+		// append value to retained 'state'
+		if( self.state){
+			self.state.push( next.value)
+		}
+
+		// notify any listeners
+		if( self.notify){
+			const old= self.notify
+			self.notify= next.done? Promise.resolve(): Deferrant()
+			old.resolve( next.value)
 		}
 		return self
 	}
-	self.value= null
-	self.done= false
 
-	//self.fork= function( ){}
+	// give an iteration of existing state data to anyone who asks
+	self[ Symbol.iterator]= function(){
+		// look at retained state
+		const iteration= self.state&& self.state[ Symbol.iterator]
+		if( iteration){
+			// & iterate through it all
+			return iteration.call( self.state)
+		}
+	}
 
 	return self
 }
